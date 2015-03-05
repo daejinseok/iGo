438b99ec48bf7c6e8499d9f0bc3d8bd95bb10546
daejin <daejin.seok@gmail.com>
2015-03-06 01:44:09 +0900

    0.4
    # 상단의 선택된 명령이 보이도록 에디터 창 추가
    # 상단의 명령 에디터 실행
    # Helper클래스 추가( 실행 및 프로세스 환경 변수)
    # ${cur_dir} 변수 추가
    # 실행경로에 쌍타옴표 처리 ( "c:\ a\a.exe" -> c:\ a\a.exe )
    # 실행에 실패할 경우 에러메세지 창 처리

--- igo/FIGo.Designer.cs
+++ igo/FIGo.Designer.cs
@@ -37,58 +37,47 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormIGo));
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textCmd = new System.Windows.Forms.TextBox();
+            this.listBox1 = new igo.ListBoxNoScr();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.AcceptsReturn = true;
             this.textBox1.AcceptsTab = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
             this.textBox1.ImeMode = System.Windows.Forms.ImeMode.Alpha;
-            this.textBox1.Location = new System.Drawing.Point(5, 5);
+            this.textBox1.Location = new System.Drawing.Point(0, 16);
             this.textBox1.Margin = new System.Windows.Forms.Padding(50);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(777, 31);
+            this.textBox1.Size = new System.Drawing.Size(800, 31);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "iGo";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
-            // 
-            // listBox1
-            // 
-            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
-            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox1.Font = new System.Drawing.Font("Malgun Gothic", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 23;
-            this.listBox1.Location = new System.Drawing.Point(5, 36);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(0);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(777, 238);
-            this.listBox1.TabIndex = 1;
-            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            this.textBox1.ImeModeChanged += new System.EventHandler(this.textBox1_ImeModeChanged);
             // 
             // linkLabel1
             // 
-            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));            
             this.linkLabel1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
             this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.linkLabel1.Location = new System.Drawing.Point(661, 261);
+            this.linkLabel1.Location = new System.Drawing.Point(0, 279);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(115, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(800, 13);
             this.linkLabel1.TabIndex = 2;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "iGo made by Daejin";
+            this.linkLabel1.Text = "Daejin =_=";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;            
+            this.linkLabel1.MouseLeave += new System.EventHandler(this.linkLabel1_MouseLeave);
+            this.linkLabel1.MouseHover += new System.EventHandler(this.linkLabel1_MouseHover);
             // 
             // notifyIcon1
             // 
@@ -100,19 +89,53 @@
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
             // 
+            // textCmd
+            // 
+            this.textCmd.AcceptsTab = true;
+            this.textCmd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.textCmd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textCmd.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textCmd.ForeColor = System.Drawing.Color.Gold;
+            this.textCmd.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.textCmd.Location = new System.Drawing.Point(0, 0);
+            this.textCmd.Name = "textCmd";
+            this.textCmd.Size = new System.Drawing.Size(800, 18);
+            this.textCmd.TabIndex = 3;
+            this.textCmd.Text = "WelCome to";
+            this.textCmd.WordWrap = false;
+            this.textCmd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textCmd_KeyDown);
+            //             
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("맑은 고딕", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(0, 48);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(800, 230);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);            
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
             // FormIGo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
-            this.ClientSize = new System.Drawing.Size(787, 279);
+            this.ClientSize = new System.Drawing.Size(800, 292);
             this.ControlBox = false;
+            this.Controls.Add(this.textCmd);            
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.CornflowerBlue;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));            
             this.ImeMode = System.Windows.Forms.ImeMode.Hangul;
             this.Margin = new System.Windows.Forms.Padding(6, 10, 6, 10);
             this.Name = "FormIGo";
@@ -133,9 +156,11 @@
 
         //private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ListBox listBox1;
+        //private System.Windows.Forms.ListBox listBox1;
+        private ListBoxNoScr listBox1;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textCmd;        
     }
 }
 
---
+++ igo/Helper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace igo
+{
+    class Helper
+    {
+        public static void Exec(string cmd)
+        {
+            string[] cmdArr = cmd.Split('|');
+            string appPath = cmdArr[0];
+
+            if (appPath.StartsWith("\"") && appPath.EndsWith("\"")) {
+                appPath = appPath.Substring(1, appPath.Length - 2);
+            }
+
+            //if (appPath.StartsWith("http", StringComparison.CurrentCultureIgnoreCase)) {
+            //    //return ( (Process.Start(appPath)).ExitCode == 0);
+            //    Process.Start(appPath);
+            //    return;
+            //}
+
+            appPath = replaceEnv(appPath);
+            FileInfo fileInfo = new FileInfo(appPath);
+
+            if (fileInfo.Exists) {
+                ProcessStartInfo startInfo = new ProcessStartInfo(appPath);
+                startInfo.WorkingDirectory = fileInfo.DirectoryName;
+
+                if (cmdArr.Length == 2) {
+                    startInfo.Arguments = replaceEnv(cmdArr[1]);
+                }
+                //return ((Process.Start(startInfo)).ExitCode == 0);
+                try {
+                    Process.Start(startInfo);
+                } catch (System.Exception e) {
+                    MessageBox.Show(e.ToString());
+                }
+            } else {
+                try {
+                    Process.Start(appPath);
+                //} catch (System.ComponentModel.Win32Exception e) {
+                //    MessageBox.Show(e.ToString());
+                //} catch (System.ObjectDisposedException e) {
+
+                //} catch (System.IO.FileNotFoundException e) {
+                } catch (System.Exception e){
+                    MessageBox.Show(e.ToString());
+                }
+                
+            }
+
+
+            return;
+            
+        }
+
+        private static string replaceEnv(string fileNameEnv)
+        {
+            var fileName = fileNameEnv;
+
+            var rgx = new Regex("%\\S+?%");
+            foreach (Match match in rgx.Matches(fileName)) {
+
+                string ev = match.Value.Substring(1, match.Value.Length - 2);
+                string evv = Environment.GetEnvironmentVariable(ev);
+
+                Debug.WriteLine(ev);
+                Debug.WriteLine(evv);
+
+                fileName = fileName.Replace(match.Value, evv);
+            }
+
+            fileName = fileName.Replace("${cur_dir}", System.Environment.CurrentDirectory);
+
+            return fileName;
+        }
+    }
+}
